6a8d7c4cf3a4f8fb68bc1f2eeb3e55d0a5e0623e
OnyxBrumeSky <mkhoury@student.42.fr>
2026-06-24 15:52:21 +0200

    few fixs

--- image_scaler.py
+++ image_scaler.py
@@ -5,32 +5,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from qiskit.circuit.library import StatePreparation
-from distribution_generator import DistributionGenerator
 from qiskit.visualization import plot_histogram
 from PIL import Image
+import math
 
 
-class PerlinGenerator:
+class ImageGenerator:
 
     def __init__(
         self,
-        resolution : (int, int) = (2, 2), 
         image_path: str,
-        shots : int = 50_000,
 ):
         self.image = Image.open(image_path).convert("RGB")
         self.array =  np.array(self.image)
         self.shots = shots
+
+        height, width = self.array[:, :, 1].astype(np.float64).shape
+        nb_bits_x = math.ceil(math.log2(height))
+        nb_bits_y = math.ceil(math.log2(width))
         
+        self.qc = [None] * 3
         for i in range(3):
-            gray = self.array[:, :, 1].astype(np.float64)
+            gray = self.array[:, :, i].astype(np.float64)
             
             height, width = gray.shape
             
             prob = gray / gray.sum()
-
-            nb_bits_x = math.ceil(math.log2(height))
-            nb_bits_y = math.ceil(math.log2(width))
             
             bit_data = []
             probs_flat = []
@@ -67,7 +67,7 @@ class PerlinGenerator:
             
             prepGate = StatePreparation(state)
             
-            self.qc.append(
+            self.qc[i].append(
                 prepGate, 
             list(np.arange(
                 start, 
@@ -75,28 +75,33 @@ class PerlinGenerator:
             
             qft_a_x = QFTGate(nb_bits_x)
             qft_a_y = QFTGate(nb_bits_y)
+            qft_a = [qft_a_x, qft_a_y]
             
             qft_b_x = QFTGate(nb_bits_x + ancilla_x)
             qft_b_y = QFTGate(nb_bits_y + ancilla_y)
-
-        for i in range(n_dimmensions):
+            qft_b = [qft_b_x, qft_b_y]
             
-            start_a = start + i * resolution
-            end_a = start_a + resolution
-            ancilla_tbl = np.arange(i * ancilla, i * ancilla + ancilla)
-            chunk = np.arange(start_a, end_a )
-            tbl = list(ancilla_tbl)
-            tbl.extend(chunk)
-            self.qc.append(qft_a, tbl[-resolution:])
-
-            for j in range(ancilla):
-                self.qc.swap(tbl[j], tbl[ancilla + j])
-            for j in range(resolution - 1):
-                self.qc.cx(tbl[-1], tbl[ancilla + j])
-
-            self.qc.append(qft_b, tbl)
-
-            self.qc.measure(tbl, list(np.arange(i * self.chunk_size, i * self.chunk_size + self.chunk_size)))
+            resolution = [nb_bits_x, nb_bits_y]
+            ancilla = [ancilla_x, ancilla_y]
+            for j in range(2):
+            
+                start_a = start + resolution[j] * j
+                end_a = start_a + resolution[j]
+                ancilla_tbl = np.arange(j * ancilla[j], j * ancilla[j] + ancilla[j])
+                chunk = np.arange(start_a, end_a )
+                tbl = list(ancilla_tbl)
+                tbl.extend(chunk)
+                print(tbl)
+                self.qc[i].append(qft_a[j], tbl[-resolution[j]:])
+    
+                for k in range(ancilla[j]):
+                    self.qc[i].swap(tbl[k], tbl[ancilla[j] + k])
+                for k in range(resolution[j] - 1):
+                    self.qc[i].cx(tbl[-1], tbl[ancilla[j] + k])
+    
+                self.qc[i].append(qft_b[j], tbl)
+    
+                self.qc[i].measure(tbl, list(np.arange(j * (resolution[j] + ancilla[j]), j * (resolution[j] + ancilla[j]) + resolution[j] + ancilla[j])))
 
     def get_circuit(self) -> QuantumCircuit: 
         return self.qc
@@ -108,34 +113,20 @@ class PerlinGenerator:
         self.counts = {k: v / shots for k, v in sorted(counts.items())}
         size = 2**(self.chunk_size)
 
-        if self.n_dims == 1 : # Simple distribution
-            self.data = self.counts
-            
-        elif self.n_dims == 2 : # 2D map
-            table = np.zeros((size,size))
-            for bitstring, count in counts.items():
-                x = int(bitstring[:self.chunk_size],2)
-                y = int(bitstring[-self.chunk_size:],2)
-                table[x][y] = count
-            self.data = (table - table.min()) / (table.max() - table.min())
-        elif self.n_dims == 3:
-            vol = np.zeros((size, size, size))
+        table = np.zeros((size,size))
+        for bitstring, count in counts.items():
+            x = int(bitstring[:self.chunk_size],2)
+            y = int(bitstring[-self.chunk_size:],2)
+            table[x][y] = count
+        self.data = (table  / table.max()) - table.min()
         
-            for bitstring, count in counts.items():
-                x = int(bitstring[:self.chunk_size], 2) % size
-                y = int(bitstring[self.chunk_size:2*self.chunk_size], 2) % size
-                z = int(bitstring[-self.chunk_size:], 2) % size
-                vol[x, y, z] = count
-        
-            self.data = (vol - vol.min()) / (vol.max() - vol.min() + 1e-9)
-            
         return self.counts
-
+    def plot()
 
 # Exemple
 if __name__ == "__main__":
-    test = PerlinGenerator(n_dimmensions=3, resolution=3, distrib="random")
+    test = ImageGenerator(image_path="ref_8.png")
     qc = test.get_circuit()
-    display(qc.draw(output="mpl"))
-    test.simulate()
-    test.plot()
+    display(qc[1].draw(output="mpl"))
+    #test.simulate()
+    #test.plot()
